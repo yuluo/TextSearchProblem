bc57aa30e60726da9898b26d3699eb998c1b066e
Yuanting Luo <yuanting.luo@viavisolutions.com>
2019-04-14 17:23:12 -0400

    Add bacis functions

--- src/search/TextSearcher.java
+++ src/search/TextSearcher.java
@@ -4,54 +4,132 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.StringWriter;
+import java.util.ArrayList;
+import java.util.List;
 
 public class TextSearcher {
 
-	/**
-	 * Initializes the text searcher with the contents of a text file.
-	 * The current implementation just reads the contents into a string 
-	 * and passes them to #init().  You may modify this implementation if you need to.
-	 * 
-	 * @param f Input file.
-	 * @throws IOException
-	 */
-	public TextSearcher(File f) throws IOException {
-		FileReader r = new FileReader(f);
-		StringWriter w = new StringWriter();
-		char[] buf = new char[4096];
-		int readCount;
-		
-		while ((readCount = r.read(buf)) > 0) {
-			w.write(buf,0,readCount);
-		}
-		
-		init(w.toString());
-	}
-	
-	/**
-	 *  Initializes any internal data structures that are needed for
-	 *  this class to implement search efficiently.
-	 */
-	protected void init(String fileContents) {
-		// TODO -- fill in implementation
-	}
-	
-	/**
-	 * 
-	 * @param queryWord The word to search for in the file contents.
-	 * @param contextWords The number of words of context to provide on
-	 *                     each side of the query word.
-	 * @return One context string for each time the query word appears in the file.
-	 */
-	public String[] search(String queryWord,int contextWords) {
-		// TODO -- fill in implementation
-		return new String[0];
-	}
+  private TextTokenizer lexer;
+  private String wordRegex = "[a-zA-Z0-9]+\'?[a-zA-Z0-9]+";
+  private List<String> tokens;
 
-	public static void main(String[] args) {
-        System.out.println("hello world");
+  /**
+   * Initializes the text searcher with the contents of a text file. The current implementation just
+   * reads the contents into a string and passes them to #init(). You may modify this implementation
+   * if you need to.
+   *
+   * @param f Input file.
+   * @throws IOException
+   */
+  public TextSearcher(File f) throws IOException {
+    FileReader r = new FileReader(f);
+    StringWriter w = new StringWriter();
+    char[] buf = new char[4096];
+    int readCount;
+
+    while ((readCount = r.read(buf)) > 0) {
+      w.write(buf, 0, readCount);
     }
+
+    // read config file here
+
+    init(w.toString());
+  }
+
+  /**
+   * Initializes any internal data structures that are needed for this class to implement search
+   * efficiently.
+   */
+  protected void init(String fileContents) {
+    // TODO -- fill in implementation
+    System.out.println(fileContents);
+    this.lexer = new TextTokenizer(fileContents, wordRegex);
+    this.tokens = new ArrayList<String>();
+
+    while (this.lexer.hasNext()) {
+      this.tokens.add(lexer.next());
+    }
+
+    /*
+       for (String token : tokens) {
+         System.out.println(token);
+    }
+    */
+  }
+
+  /** find all the index of the queryword */
+  protected Integer[] findIndex(String queryWord) {
+    String lowerCaseQuery = queryWord.toLowerCase();
+    List<Integer> indexes = new ArrayList<Integer>();
+    for (int i = 0; i < this.tokens.size(); i++) {
+      String token = this.tokens.get(i).toLowerCase();
+      if (token.equals(lowerCaseQuery)) {
+        indexes.add(i);
+      }
+    }
+
+    return (Integer[]) indexes.toArray(new Integer[indexes.size()]);
+  }
+
+  protected String getContextWords(int index, int num) {
+    int numOfContext = 0;
+    int direction = num > 0 ? 1 : -1;
+    int start = index + direction;
+    String context = "";
+
+    while (numOfContext != Math.abs(num)) {
+      String token = this.tokens.get(start);
+      if (this.lexer.isWord(token)) {
+        numOfContext++;
+      }
+
+      if (num > 0) {
+        context += token;
+      } else {
+        context = token + context;
+      }
+      start += direction;
+
+      if (start < 0 || start == this.tokens.size()) {
+        numOfContext = Math.abs(num);
+      }
+    }
+
+    return context;
+  }
+
+  /**
+   * @param queryWord The word to search for in the file contents.
+   * @param contextWords The number of words of context to provide on each side of the query word.
+   * @return One context string for each time the query word appears in the file.
+   */
+  public String[] search(String queryWord, int contextWords) {
+    // TODO -- fill in implementation
+    Integer[] indexes = findIndex(queryWord);
+    List<String> results = new ArrayList<String>();
+    for (int index : indexes) {
+      System.out.println(index);
+      String context =
+          getContextWords(index, contextWords * -1)
+              + this.tokens.get(index)
+              + getContextWords(index, contextWords);
+      results.add(context);
+    }
+
+    return (String[]) results.toArray(new String[results.size()]);
+  }
+
+  public static void main(String[] args) throws Exception {
+    File file = new File("files/short_excerpt.txt");
+    TextSearcher searcher = new TextSearcher(file);
+    String[] results = searcher.search("Naturalists", 1);
+
+    for (String result : results) {
+      System.out.println(result);
+    }
+
+    System.out.println("end");
+  }
 }
 
 // Any needed utility classes can just go in this file
-
